f60200ff6a598efb0013ac88505bf11231ca2236
unknown <aldrichzefanya3@gmail.com>
2023-12-18 12:31:45 +0700

    Fixing import packages

--- run.py
+++ run.py
@@ -2,4 +2,4 @@ from src.main import app
 import uvicorn
 
 if __name__ == "__main__":
-    uvicorn.run("run:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("run:app", port=8000, reload=True)
